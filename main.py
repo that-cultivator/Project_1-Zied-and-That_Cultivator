4c9e3d41120cfab41b2c774ff1fbcb1f79af151f
zied23e <156387590+zied23e@users.noreply.github.com>
2024-09-25 22:30:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import argparse
 import mysql.connector
-
+#connects the code to the "mysql" db 
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -8,7 +9,7 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
-
+#create the table if it doesn't exist
 cursor.execute("""CREATE TABLE IF NOT EXISTS wordlist (
                id INTEGER AUTO_INCREMENT PRIMARY KEY, 
                word TEXT
@@ -17,57 +18,71 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS wordlist (
 
 conn.commit()  # saves the progress in the database
 
-while True:
-    print("\nMenu:")
-    print("1. Add Column")
-    print("2. Insert A Word")
-    print("3. View All Data")
-    print("4. Delete a column")
+#create commands with the help of the argparse module
+parser = argparse.ArgumentParser()
+parser.add_argument("-c","--create", dest="create", help="creats a new column in the database")
+parser.add_argument("-a","--addwords", dest="add", help="adds a new word or list of words to the database")
+parser.add_argument("-col", "--column", dest="column", help="Specifies the column to add the word to")
+parser.add_argument("-s","--show", dest="show", help="shows the contents of the database", action="store_true")
+parser.add_argument("-d","--delete", help="deletes an item from the database")
+parser.add_argument("-u", "--url",dest="url", help="specify the url you want to fuzz")
+args=parser.parse_args()
 
-    cmd = int(input("Enter a command number: "))
-
-    if cmd == 1:
-        cname = input("Enter the name of the column: ")
-        query = f"ALTER TABLE wordlist ADD `{cname}` TEXT"
-        cursor.execute(query)
-        print(f"Added a new column '{cname}' to table 'wordlist'.")
-        conn.commit()
-
-    elif cmd == 2:
-        word = input("Enter the word you want to insert: ")
-        cname = input("Please enter name of the column: ")
-
-        if word and cname:
-            try:
-                query = f"INSERT INTO wordlist (`{cname}`) VALUES (%s)"
-                cursor.execute(query, (word,))
-                conn.commit()
-                print(f"Inserted '{word}' into column '{cname}'.")
-                
-            except mysql.connector.Error as err:
-                print(f"Error: {err}")
-                print("Please enter a valid column name!")
-
-    elif cmd == 3:
-        cursor.execute("SELECT * FROM wordlist")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    elif cmd == 4: 
-        cname = input("Enter name of the column you want to delete: ")
-        query = f"ALTER TABLE wordlist DROP COLUMN {cname}"
+ #check if the creat command has been called
+if args.create:
+        cname = args.create
+        #if yes take the value or string provided with it and create a new column with that name
         try:
-            if cname != "id":
-                cursor.execute(query)
-                print(f"Successfully deleted column: {cname}")
-            else:
-                print("You cannot delete the 'id' column!")
-        except:
-            print("Please enter a valid column name!")   
+            query = f"ALTER TABLE wordlist ADD `{cname}` TEXT"
+            cursor.execute(query)
+            print(f"Added a new column '{cname}' to table 'wordlist'.")
+            conn.commit()
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
 
+#add words to a column by using the -col to define the column
+elif args.add :
+    word = args.add
+    cname=args.column
+    if word and cname:
+        try:
+            query = f"INSERT INTO wordlist (`{cname}`) VALUES (%s)"
+            cursor.execute(query, (word,))
+            conn.commit()
+            print(f"Inserted '{word}' into column '{cname}'.")
+                
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            print("Please enter a valid column name!")
+#if -s is called show all words in the wordlist
+elif args.show:
+    cursor.execute("SELECT * FROM wordlist")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+#delete/drop the column
+elif args.delete: 
+    cname = args.delete
+    query = f"ALTER TABLE wordlist DROP COLUMN {cname}"
+    try:
+        if cname != "id":
+            cursor.execute(query)
+            print(f"Successfully deleted column: {cname}")
+        else:
+            print("You cannot delete the 'id' column!")
+    except:
+        print("Please enter a valid column name!")   
 
-    else:
-        print("Invalid command.")
+#taking the url and replacing it with the words from the db
 
+elif args.url:
+    cursor.execute("select * from wordlist")
+    words = cursor.fetchall()
+    for word in words:
+        word = word[0]
+        replace = args.url.replace("*", word)
+        print(replace)
 
+#none of the above
+else:
+    print("Invalid command.")
